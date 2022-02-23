8fe576dd7dd89dcda303fe42964cfcfe7a518334
Kevin Yang <kgyang@stanford.edu>
2022-02-23 00:35:00 -0800

    Updated convert function to convert to questions Added questions & yes/no answers to dataset

--- beliefbank_data/convert_to_sentences.py
+++ beliefbank_data/convert_to_sentences.py
@@ -14,7 +14,7 @@ def starts_with_vowel_sound(word, pronunciations=cmudict.dict()):
 
 
         
-f = open("silver_facts.json")
+f = open("calibration_facts.json")
 data = json.load(f)
 
 possibilities = []
@@ -34,10 +34,11 @@ connector = {"IsA": [" is ", " is not "],
             "CapableOf": [" is able to " , " is not able to "],
             "MadeOf": [" is made of ", " is not made of "], 
             "HasProperty": [" has the property of being ", " does not have the property of being "],
-            "HasPart": [" is part of ", " is not part of "]
+            "HasPart": [" has ", " does not have "]
             }
 
-with open('silver_sentences.csv', 'w', newline='') as csvfile:
+### Generate questions
+with open('calibration_sentences.csv', 'w', newline='') as csvfile:
     for i in data:
         for property in data[i]:
             true = (data[i][property] == "yes")
@@ -53,11 +54,37 @@ with open('silver_sentences.csv', 'w', newline='') as csvfile:
                 subjectArticle = "An " if starts_with_vowel_sound(i) else "A "
                 sentence = subjectArticle + i + (connector[category][0] if true else connector[category][1]) + recipient + "."
                 
-            else:
-                subjectArticle = "an " if starts_with_vowel_sound(i) else "a "
-                objectArticle = "An " if starts_with_vowel_sound(recipient) else "A "
-                sentence = objectArticle + i + (connector[category][0] if true else connector[category][1]) + subjectArticle + recipient + "."
+            elif (category  == "HasPart"):
+                subjectArticle = "An " if starts_with_vowel_sound(i) else "A "
+                objectArticle = "an " if starts_with_vowel_sound(recipient) else "a "
+                sentence = subjectArticle + i + (connector[category][0] if true else connector[category][1]) + objectArticle + recipient + "."
 
             print(sentence)
             writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             writer.writerow([str(sentence)])
+
+question_connector = {"IsA": ["Is", ""], 
+             "HasA": ["Does", " have a "],
+            "CapableOf": ["Is", " capable of "],
+            "MadeOf": ["Is", " made of "], 
+            "HasProperty": ["Does", " have the property of being "],
+            "HasPart": ["Does", " have a "]
+            }
+
+### Generate Questions and Answers
+with open('calibration_questions.csv', 'w', newline='') as csvfile:
+    for i in data:
+        for property in data[i]:
+            true = (data[i][property] == "yes")
+            category, recipient = property.split(",")
+
+            subjectArticle = " an " if starts_with_vowel_sound(i) else " a "
+            objectArticle = " an " if starts_with_vowel_sound(recipient) else " a "
+
+            answer = "Yes" if true else "No"
+            question = question_connector[category][0] + subjectArticle + i + question_connector[category][1] + \
+                (objectArticle if category in ["IsA"] else "") + recipient + "? " + answer
+
+            print(question)
+            writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow([str(question)])
